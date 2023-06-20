78b7f23290800e14c9ecddb76b6985459fe5d70d
nada <sergiodanilomosqueraquigua@gmail.com>
2023-06-20 02:42:10 -0500

    v1.3
    Ventana de manager completada, pero sin funcionalidades

---
+++ src/main/kotlin/com/xsergiodm/controller/Manager_controller.kt
@@ -0,0 +1,17 @@
+package com.xsergiodm.controller
+
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+
+class Manager_controller {
+
+    @FXML
+    private lateinit var btInfo: Button
+
+    @FXML
+    fun informacion(event: ActionEvent) {
+
+    }
+
+}
